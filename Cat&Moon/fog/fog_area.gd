239e14049d38ce9a7b1cf5dc3de57651d6630e90
Trylat <40700996+Trylat@users.noreply.github.com>
2023-08-03 23:05:59 -0400

    Cat animation & state machine ADD SONDS n stuff (#19)
    * Ajout Animations fall, walk et iddle avec le nouveau sprite
    
    * Animation Sweeth
    
    * truc before merge
    
    * Update Test level 1.tscn
    
    * SFX
    
    * fix bug where hiss do crash on hiss sound playing
    
    * add sounds again
    
    * add more sfx
    
    ---------
    
    Co-authored-by: drick0230 <61423988+drick0230@users.noreply.github.com>

--- Cat&Moon/fog/fog_area.gd
+++ Cat&Moon/fog/fog_area.gd
@@ -2,5 +2,11 @@ class_name FogArea extends Area2D
 
 signal hiss_received
 
+func _ready():
+	$EvilCloudWisperSXF.play()
+
 func on_hiss():
 	hiss_received.emit()
+	$EvilCloudWisperSXF.stop()
+	$CloudDamageFromCatSFX.play()
+	$EvilCloudWisperSXF.play()
